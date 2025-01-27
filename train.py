708e054521309d831131a115628a82a453b30520
chf3-test <2213096302@qq.com>
2025-01-27 19:57:38 +0800

    modified:   train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO("yolo11n.yaml")  # build a new model from YAML
-model = YOLO("yolo11l.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11m.pt")  # load a pretrained model (recommended for training)
 # model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
 # Train the model
